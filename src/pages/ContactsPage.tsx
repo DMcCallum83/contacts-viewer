70850fcedb16fcf8aece3e70f53ae3cb53d75b79
David McCallum <david_mccallum83@yahoo.com>
2024-03-23 21:03:21 +0000

    update page title and logo

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -2,9 +2,9 @@ import { Contacts } from "../components/Contacts/Contacts";
 
 export function ContactsPage() {
   return (
-    <>
+    <main>
       <h1>Contacts List</h1>
       <Contacts />
-    </>
+    </main>
   );
 }

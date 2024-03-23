a8de06533b98a5a871488730eb9fa2a95734edfb
David McCallum <david_mccallum83@yahoo.com>
2024-03-23 21:35:53 +0000

    minor styling

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,10 +1,11 @@
 import { Contacts } from "../components/Contacts/Contacts";
+import styles from "./_shared.module.scss";
 
 export function ContactsPage() {
   return (
-    <main>
-      <h1>Contacts List</h1>
+    <section className={styles.section}>
+      <h1 className={styles.sectionTitle}>Contacts List</h1>
       <Contacts />
-    </main>
+    </section>
   );
 }

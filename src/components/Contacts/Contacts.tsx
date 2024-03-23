c850cea902e1fe3e5a5a4854e1ec8590652a9864
David McCallum <david_mccallum83@yahoo.com>
2024-03-23 21:35:44 +0000

    use new hook to render data and handle loading and error state with basic messages

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,3 +1,14 @@
+import { useGetContacts } from "../../hooks/useGetContacts";
+
 export function Contacts() {
-  return <span>list here</span>;
+  const { data, isLoading, isError } = useGetContacts();
+
+  if (isLoading) {
+    return <span>LOADING...</span>;
+  }
+  if (isError) {
+    return <span>Whoops something went wrong...</span>;
+  }
+
+  return data?.map((contact) => <span key={contact.name}>{contact.name}</span>);
 }
